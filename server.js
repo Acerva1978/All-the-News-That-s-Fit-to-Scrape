800156e74a59d6258e43acc9b702c5e4e10641bf
Adrian Cervantes <lagos86@hotmail.com>
2019-08-10 14:19:34 -0500

    heroku connetion made it

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var mongoose = require('mongoose');
 
 var MONGODB_URI = process.env.MONGODB_URI;
 
-var MONGODB_URI = MONGODB_URI || 'mongodb://localhost/newstaker';
+var MONGODB_URI = MONGODB_URI || REMOTE;
+
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
